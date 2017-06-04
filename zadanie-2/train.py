c7eed8f35da9f18e32f16592561dc2c47812b9a5
PiotrJander <pjander2@gmail.com>
2017-06-04 11:55:00 +0200

    Refactors, preloads data as variable

--- zadanie-2/train.py
+++ zadanie-2/train.py
@@ -1,90 +1,68 @@
-# Typical setup to include TensorFlow.
-import base64
+from __future__ import print_function, generators
+
 import datetime
+import random
+
 import tensorflow as tf
 import os
 
 LOG_DIR = 'logs/' + datetime.datetime.now().strftime("%B-%d-%Y;%H:%M")
 # DATA_SET_SIZE = 10593
 DATA_SET_SIZE = 20
+VALIDATION_SET_SIZE = 10
 IMAGES_DIR = '/data/spacenet2/images/'
 HEATMAPS_DIR = '/data/spacenet2/heatmaps/'
 
-# Make a queue of file names including all the JPEG images files in the relative
-# image directory.
-# images_filename_queue = tf.train.string_input_producer(
-#     tf.train.match_filenames_once("/data/spacenet2/images/*.jpg"), shuffle=False)
-# heatmaps_filename_queue = tf.train.string_input_producer(
-#     tf.train.match_filenames_once("/data/spacenet2/heatmaps/*.jpg"), shuffle=False)
 
-# Read an entire image file which is required since they're JPEGs, if the images
-# are too large they could be split in advance to smaller files or use the Fixed
-# reader to split up the file.
-# image_reader = tf.WholeFileReader()
+def train_set_size():
+    return DATA_SET_SIZE - VALIDATION_SET_SIZE
 
 
-def read_and_decode(folder, f):
-    with open(os.path.join(folder, f)) as file:
-        # string = base64.b64encode(file.read())
-        return tf.image.decode_jpeg(file.read(), ratio=2)
+def read_and_decode(folder, filename):
+    path = os.path.join(folder, filename)
+    with open(path) as f:
+        return tf.image.decode_jpeg(f.read(), ratio=2)
 
 
-images = []
-heatmaps = []
-
-for _, imagename, heatmapname in zip(xrange(DATA_SET_SIZE), sorted(os.listdir(IMAGES_DIR)), sorted(os.listdir(HEATMAPS_DIR))):
-    print imagename
-    print heatmapname
-    images.append(read_and_decode(IMAGES_DIR, imagename))
-    heatmaps.append(read_and_decode(HEATMAPS_DIR, heatmapname))
-
-images = tf.stack(images)
-heatmaps = tf.stack(heatmaps)
+def create_partition_vector():
+    partitions = [0] * DATA_SET_SIZE
+    partitions[:VALIDATION_SET_SIZE] = [1] * VALIDATION_SET_SIZE
+    random.shuffle(partitions)
+    return partitions
 
 
-# Read a whole file from the queue, the first returned value in the tuple is the
-# filename which we are ignoring.
-# images = []
-# heatmaps = []
-# for i in xrange(DATA_SET_SIZE):
-#     images.append(read_and_decode(images_filename_queue))
-#     heatmaps.append(read_and_decode(heatmaps_filename_queue))
-# images = tf.stack(images)
-# heatmaps = tf.stack(heatmaps)
+class Model(object):
+    def __init__(self):
+        super(Model, self).__init__()
 
-images_summary_op = tf.summary.image("images", images[:10])
-heatmaps_summary_op = tf.summary.image("heatmaps", heatmaps[:10])
-all_summaries = tf.summary.merge_all()
+        self.images = []
+        self.heatmaps = []
 
-# Decode the image as a JPEG file, this will turn it into a Tensor which we can
-# then use in training.
-# image = tf.image.decode_jpeg(image_file)
-# image_batch = tf.expand_dims(image, 0)
-# summary_op = tf.summary.image("plot", image_batch)
-# all_summaries = tf.summary.merge_all()
+        self.load_images()
 
-# Start a new session to show example output.
-with tf.Session() as sess:
-    writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
+        with tf.name_scope('input'):
+            # shape = [DATA_SET_SIZE, 325, 325, 3]
+            partitions = create_partition_vector()
 
-    # Required to get the filename matching to run.
-    tf.initialize_all_variables().run()
+            train_images_value, validate_images_value = tf.dynamic_partition(self.images, partitions, 2)
+            train_heatmaps_value, validate_heatmaps_value = tf.dynamic_partition(self.heatmaps, partitions, 2)
 
-    # Coordinate the loading of image files.
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(coord=coord)
+            self.train_images = tf.Variable(train_images_value, trainable=False)
+            self.train_heatmaps = tf.Variable(train_heatmaps_value, trainable=False)
+            self.validate_images = tf.Variable(validate_images_value, trainable=False)
+            self.validate_heatmaps = tf.Variable(validate_heatmaps_value, trainable=False)
 
-    # Get an image tensor and print its value.
-    # image_tensor = sess.run([image])
-    # print(image_tensor)
+    def load_images(self):
+        images_filenames = sorted(os.listdir(IMAGES_DIR))
+        heatmaps_filenames = sorted(os.listdir(HEATMAPS_DIR))
+        data = zip(range(DATA_SET_SIZE), images_filenames, heatmaps_filenames)
+        for _, image_name, heatmap_name in data:
+            self.images.append(read_and_decode(IMAGES_DIR, image_name))
+            self.heatmaps.append(read_and_decode(HEATMAPS_DIR, heatmap_name))
+        self.images = tf.stack(self.images)
+        self.heatmaps = tf.stack(self.heatmaps)
 
-    # shape = sess.run(tf.shape(images))
-    # print shape
-
-    summary = sess.run(all_summaries)
-    writer.add_summary(summary)
-    writer.close()
-
-    # Finish off the filename queue coordinator.
-    coord.request_stop()
-    coord.join(threads)
+    def train(self):
+        with tf.Session() as sess:
+            # writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
+            tf.initialize_all_variables().run()
