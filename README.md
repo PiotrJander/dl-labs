6fb9ab2ddb25eea5c7de98c6507e53181b9befb3
PiotrJander <pjander2@gmail.com>
2017-06-04 17:11:14 +0200

    Adds computation graph

--- README.md
+++ README.md
@@ -1,39 +1,10 @@
-now we'd like to take batches
+okay now we have data
 
-make batch an op
+we take batches of N, say 2 or 50
 
-take batch from train and validate
+first write those batches to tboard
 
-write images to summary
+we want to resize images to 256
 
-here the object is a singleton
-used as a namespace
-
-why not simply use global namespace of file?
-
-or of function
-
-now batch relies on queues
-
-but queues are not needed maybe
-
-what if we take 
-
-have a counter var
-
-take slice of 50 images in each iter
-
-then do c += 50
-
-one epoch = a number of batches
-
-could also pass the counter as placeholder
-
-but write summary first
-
-
-
-
-
-
+but when we compute accuracy, resize back
 
--- zadanie-2/train.py
+++ zadanie-2/train.py
@@ -1,6 +1,11 @@
 """
 TODO should we use queues
 TODO should we randomize batches
+TODO do I leverage tensor shape type checking?
+TODO name scope vs var scope
+TODO For conv transpose use kernel size divisible by stride, or (possibly better option) upsample by nearest neighbour and then do regular convolution
+
+TODO conv heatmaps: give 3 dims, one hot = true
 """
 
 from __future__ import print_function, generators
@@ -15,12 +20,13 @@ LOG_DIR = 'logs/' + datetime.datetime.now().strftime("%B-%d-%Y;%H:%M")
 # DATA_SET_SIZE = 10593
 DATA_SET_SIZE = 20
 VALIDATION_SET_SIZE = 10
-IMAGES_DIR = '/data/spacenet2/images/'
-HEATMAPS_DIR = '/data/spacenet2/heatmaps/'
-
-
-def train_set_size():
-    return DATA_SET_SIZE - VALIDATION_SET_SIZE
+TRAIN_SET_SIZE = DATA_SET_SIZE - VALIDATION_SET_SIZE
+DATA_DIR = os.environ['SPACENET'] or '/data/spacenet2/'
+IMAGES_DIR = os.path.join(DATA_DIR, 'images')
+HEATMAPS_DIR = os.path.join(DATA_DIR, 'heatmaps')
+BATCH_SIZE = 3
+IMAGE_SIZE = 256
+CHANNELS = 3
 
 
 def read_and_decode(folder, filename):
@@ -37,55 +43,55 @@ def create_partition_vector():
 
 
 def setup():
-    images = []
-    heatmaps = []
+    images_before_resizing = []
+    heatmaps_before_resizing = []
 
     images_filenames = sorted(os.listdir(IMAGES_DIR))
     heatmaps_filenames = sorted(os.listdir(HEATMAPS_DIR))
     data = zip(range(DATA_SET_SIZE), images_filenames, heatmaps_filenames)
 
     for _, image_name, heatmap_name in data:
-        images.append(read_and_decode(IMAGES_DIR, image_name))
-        heatmaps.append(read_and_decode(HEATMAPS_DIR, heatmap_name))
-    images = tf.stack(images)
-    heatmaps = tf.stack(heatmaps)
+        images_before_resizing.append(read_and_decode(IMAGES_DIR, image_name))
+        heatmaps_before_resizing.append(read_and_decode(HEATMAPS_DIR, heatmap_name))
+    images_before_resizing = tf.stack(images_before_resizing)
+    heatmaps_before_resizing = tf.stack(heatmaps_before_resizing)
+
+    images = tf.image.resize_images(images_before_resizing, [IMAGE_SIZE, IMAGE_SIZE])
+    heatmaps = tf.image.resize_images(heatmaps_before_resizing, [IMAGE_SIZE, IMAGE_SIZE])
 
     with tf.name_scope('input'):
-        # train_shape = [train_set_size(), 325, 325, 3]
-        # validate_shape = [VALIDATION_SET_SIZE, 325, 325, 3]
         partitions = create_partition_vector()
 
         train_images_value, validate_images_value = tf.dynamic_partition(images, partitions, 2)
         train_heatmaps_value, validate_heatmaps_value = tf.dynamic_partition(heatmaps, partitions, 2)
 
-        # train_images_value1 = tf.constant(
-        #     train_images_value,
-        #     dtype=tf.uint8, shape=)
-
-        def data_var(data):
-            return tf.Variable(data, trainable=False, validate_shape=False)
+        def data_var(init):
+            return tf.Variable(init, trainable=False, validate_shape=False)
 
         train_images = data_var(train_images_value)
         train_heatmaps = data_var(train_heatmaps_value)
         validate_images = data_var(validate_images_value)
         validate_heatmaps = data_var(validate_heatmaps_value)
 
-        # images_summary_op = tf.summary.image("train_images", train_images[:10])
-        # heatmaps_summary_op = tf.summary.image("train_heatmaps", train_heatmaps[:10])
-        # all_summaries = tf.summary.merge_all()
-        image_summaries = tf.summary.merge(tf.summary.image(str(i), tensor[:3])
-                                           for i, tensor
-                                           in enumerate([train_images, train_heatmaps, validate_images, validate_heatmaps])
-                                           )
+        train_images.set_shape([TRAIN_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        train_heatmaps.set_shape([TRAIN_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        validate_images.set_shape([VALIDATION_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        validate_heatmaps.set_shape([VALIDATION_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
 
-        # Where to take the slice for the batch
-        # batch_start = tf.placeholder(tf.uint16)
+        batch_start = tf.placeholder(tf.int32, shape=[])
+        batch_images = tf.slice(train_images, [batch_start, 0, 0, 0], [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        batch_heatmaps = tf.slice(train_heatmaps, [batch_start, 0, 0, 0], [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
 
         with tf.Session() as sess:
             writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
             tf.initialize_all_variables().run()
 
-            summary = sess.run(image_summaries)
+            # shape = sess.run(tf.shape(batch_images), feed_dict={batch_start: 19})
+            # print(shape)
+
+            # summary = sess.run(image_summaries('first'), feed_dict={batch_start: 0})
+            # writer.add_summary(summary)
+            summary = sess.run(image_summaries('second'), feed_dict={batch_start: 8})
             writer.add_summary(summary)
             writer.close()
 
@@ -94,10 +100,125 @@ if __name__ == '__main__':
     setup()
 
 
+def conv(features, in_channels, out_channels, kernel_size=3, name='conv'):
+    with tf.variable_scope(name):
+        stride = 1
+        weights = tf.get_variable("weights", [kernel_size, kernel_size, in_channels, out_channels],
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1))
+        return tf.nn.conv2d(features, weights, strides=[1, stride, stride, 1], padding='SAME')
 
 
+def conv_relu(features, in_channels, out_channels, name='conv_relu'):
+    with tf.name_scope(name):
+        return tf.nn.relu(conv(features, in_channels, out_channels))
 
 
+def bn_conv_relu(features, in_channels, out_channels, name='bn_conv_relu'):
+    with tf.variable_scope(name):
+        scale = tf.get_variable("scale", [in_channels],
+                                initializer=tf.constant_initializer(1.0))
+        offset = tf.get_variable("beta", [in_channels],
+                                 initializer=tf.constant_initializer(0.0))
+        mean, variance = tf.nn.moments(features, [0, 1, 2], keep_dims=True)
+        out = tf.nn.batch_normalization(features, mean, variance, scale, offset, variance_epsilon=1e-4)
+        return conv_relu(out, in_channels, out_channels)
 
+
+def max_pool(features):
+    return tf.nn.max_pool(features, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')  # TODO maybe VALID
+
+
+def bn_conv_relu_3_maxpool(features, channels, name='bn_conv_relu_3_maxpool'):
+    with tf.name_scope(name):
+        out = bn_conv_relu(features, channels, channels)
+        out_skip_conn = bn_conv_relu(out, channels, channels)
+        out = bn_conv_relu(out_skip_conn, channels, channels)
+        out = max_pool(out)
+        return out_skip_conn, out
+
+
+def upconv(features, in_channels, out_channels, name='upconv'):
+    with tf.variable_scope(name):
+        stride = 2
+        weights = tf.get_variable("weights", [3, 3, out_channels, in_channels],
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1))
+        return tf.nn.conv2d_transpose(features, weights, strides=[1, stride, stride, 1], padding='SAME')
+
+
+def bn_upconv_relu(features, in_channels, out_channels, name='bn_upconv_relu'):
+    with tf.variable_scope(name):
+        scale = tf.get_variable("scale", [in_channels],
+                                initializer=tf.constant_initializer(1.0))
+        offset = tf.get_variable("beta", [in_channels],
+                                 initializer=tf.constant_initializer(0.0))
+        mean, variance = tf.nn.moments(features, [0, 1, 2], keep_dims=True)
+        out = tf.nn.batch_normalization(features, mean, variance, scale, offset, variance_epsilon=1e-4)
+        return tf.nn.relu(upconv(out, in_channels, out_channels))
+
+
+def concat(features_down, features_up, name='concat'):
+    with tf.name_scope(name):
+        return tf.concat([features_down, features_up], 3)
+
+
+def concat_bn_conv_relu_2(features_down, features_up, name='concat_bn_conv_relu_2'):
+    with tf.name_scope(name):
+        out = concat(features_down, features_up)
+        out = bn_conv_relu(out, 128, 96)
+        return bn_conv_relu(out, 96, 64)
+
+
+def concat_bn_conv_relu_2_bn_upconv_relu(features_down, features_up, name='concat_bn_conv_relu_2_bn_upconv_relu'):
+    with tf.name_scope(name):
+        out = concat_bn_conv_relu_2(features_down, features_up)
+        return bn_upconv_relu(out, 64, 64)
+
+
+def convout(features, in_channels, name='convout'):
+    return conv(features, in_channels, 1, kernel_size=1, name=name)
+
+
+def conv_net(features):
+    with tf.name_scope('down1'):
+        features_down1 = conv_relu(features, 3, 64)
+
+    with tf.name_scope('down2'):
+        features_down2_skip_conn = bn_conv_relu(features_down1, 64, 64)
+        features_down2 = bn_conv_relu(features_down2_skip_conn, 64, 64)
+        features_down2 = max_pool(features_down2)
+
+    features_down3_skip_conn, features_down3 = bn_conv_relu_3_maxpool(features_down2, 64, name='down3')
+    features_down4_skip_conn, features_down4 = bn_conv_relu_3_maxpool(features_down3, 64, name='down4')
+    features_down5_skip_conn, features_down5 = bn_conv_relu_3_maxpool(features_down4, 64, name='down5')
+    features_down6_skip_conn, features_down6 = bn_conv_relu_3_maxpool(features_down5, 64, name='down6')
+
+    with tf.name_scope('up1'):
+        features_up1 = bn_conv_relu(features_down6, 64, 64)
+        features_up1 = bn_conv_relu(features_up1, 64, 64)
+        features_up1 = bn_upconv_relu(features_up1, 64, 64)
+
+    features_up2 = concat_bn_conv_relu_2_bn_upconv_relu(features_down6_skip_conn, features_up1, name='up2')
+    features_up3 = concat_bn_conv_relu_2_bn_upconv_relu(features_down5_skip_conn, features_up2, name='up3')
+    features_up4 = concat_bn_conv_relu_2_bn_upconv_relu(features_down4_skip_conn, features_up3, name='up4')
+    features_up5 = concat_bn_conv_relu_2_bn_upconv_relu(features_down3_skip_conn, features_up4, name='up5')
+
+    with tf.name_scope('up6'):
+        features_up6 = concat_bn_conv_relu_2(features_down2_skip_conn, features_up5)
+        features_up6 = convout(features_up6, 64)
+
+    return features_up6
+
+
+# # Construct model
+# pred = conv_net(x)
+#
+# # Define loss and optimizer
+# # Applies softmax internally
+# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+# optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+#
+# # Evaluate model
+# correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+# accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 
