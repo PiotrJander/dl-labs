11c21e2230d36bff946fa37d3906484afb29276f
PiotrJander <pjander2@gmail.com>
2017-06-06 09:36:13 +0200

    Refactors to use queues

--- zadanie-2/train.py
+++ zadanie-2/train.py
@@ -16,6 +16,7 @@ IMAGES_DIR = os.path.join(DATA_DIR, 'images')
 HEATMAPS_DIR = os.path.join(DATA_DIR, 'heatmaps')
 BATCH_SIZE = int(os.environ.get('BATCH_SIZE') or 10)
 AUGMENTED_BATCH_SIZE = 8 * BATCH_SIZE
+HALF_IMAGE_SIZE = 325
 IMAGE_SIZE = 256
 CHANNELS = 3
 LEARNING_RATE = 0.001
@@ -153,19 +154,6 @@ def conv_net(features):
     return features_up6
 
 
-def read_image(folder, filename):
-    path = os.path.join(folder, filename)
-    with open(path) as f:
-        return f.read()
-
-
-def create_partition_vector():
-    partitions = [0] * DATA_SET_SIZE
-    partitions[:VALIDATION_SET_SIZE] = [1] * VALIDATION_SET_SIZE
-    random.shuffle(partitions)
-    return partitions
-
-
 def augment_image(image):
     r = tf.image.rot90
     s = tf.image.flip_left_right
@@ -208,77 +196,64 @@ def augment_many(images):
     return tf.reshape(augmented_batch, [-1, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
 
 
-def read_images():
-    images = []
-    heatmaps = []
+def get_file_names():
+    images_filenames = os.listdir(IMAGES_DIR)
+    random.shuffle(images_filenames)
+    train = images_filenames[:TRAIN_SET_SIZE]
+    validate = images_filenames[TRAIN_SET_SIZE:]
+    assert len(validate) == VALIDATION_SET_SIZE
 
-    images_filenames = sorted(os.listdir(IMAGES_DIR))
-    # heatmaps_filenames = sorted(os.listdir(HEATMAPS_DIR))
-    data = zip(range(DATA_SET_SIZE), images_filenames)
+    train_images_names = [os.path.join(IMAGES_DIR, name) for name in train]
+    train_heatmaps_names = [os.path.join(HEATMAPS_DIR, name) for name in train]
+    validate_images_names = [os.path.join(IMAGES_DIR, name) for name in validate]
+    validate_heatmaps_names = [os.path.join(HEATMAPS_DIR, name) for name in validate]
 
-    for _, filename in data:
-        images.append(read_image(IMAGES_DIR, filename))
-        heatmaps.append(read_image(HEATMAPS_DIR, filename))
-
-    return images, heatmaps
+    return train_images_names, train_heatmaps_names, validate_images_names, validate_heatmaps_names
 
 
 class Model(object):
     def __init__(self):
         super(Model, self).__init__()
 
-        with tf.name_scope('input'):
-            images_initializer = tf.placeholder(dtype=tf.string, shape=[DATA_SET_SIZE])
-            heatmaps_initializer = tf.placeholder(dtype=tf.string, shape=[DATA_SET_SIZE])
+        train_images_names, train_heatmaps_names, validate_images_names, validate_heatmaps_names = get_file_names()
 
-            def decode(image):
-                return tf.image.decode_jpeg(image, ratio=2)
+        train_images_queue = tf.train.string_input_producer(train_images_names, shuffle=False)
+        train_heatmaps_queue = tf.train.string_input_producer(train_heatmaps_names, shuffle=False)
+        validate_images_queue = tf.train.string_input_producer(validate_images_names, shuffle=False)
+        validate_heatmaps_queue = tf.train.string_input_producer(validate_heatmaps_names, shuffle=False)
 
-            images_before_resizing = tf.map_fn(decode, images_initializer, dtype=tf.uint8)
-            heatmaps_before_resizing = tf.map_fn(decode, heatmaps_initializer, dtype=tf.uint8)
+        image_reader = tf.WholeFileReader()
 
-            images = tf.image.resize_images(images_before_resizing, [IMAGE_SIZE, IMAGE_SIZE])
-            heatmaps = tf.image.resize_images(heatmaps_before_resizing, [IMAGE_SIZE, IMAGE_SIZE])
+        _, train_image_file = image_reader.read(train_images_queue)
+        _, train_heatmap_file = image_reader.read(train_heatmaps_queue)
+        _, validate_image_file = image_reader.read(validate_images_queue)
+        _, validate_heatmap_file = image_reader.read(validate_heatmaps_queue)
 
-            partitions = create_partition_vector()
+        train_image = tf.image.decode_jpeg(train_image_file, ratio=2)
+        train_heatmap = tf.image.decode_jpeg(train_heatmap_file, ratio=2)
+        validate_image = tf.image.decode_jpeg(train_image_file, ratio=2)
+        validate_heatmap = tf.image.decode_jpeg(train_image_file, ratio=2)
 
-            train_images_value, validate_images_value = tf.dynamic_partition(images, partitions, 2)
-            train_heatmaps_value, validate_heatmaps_value = tf.dynamic_partition(heatmaps, partitions, 2)
+        train_image = tf.image.resize_images(train_image, [IMAGE_SIZE, IMAGE_SIZE])
+        train_heatmap = tf.image.resize_images(train_heatmap, [IMAGE_SIZE, IMAGE_SIZE])
+        validate_image = tf.image.resize_images(validate_image, [IMAGE_SIZE, IMAGE_SIZE])
+        validate_heatmap = tf.image.resize_images(validate_heatmap, [IMAGE_SIZE, IMAGE_SIZE])
 
-            def data_var(init):
-                return tf.Variable(init, trainable=False, validate_shape=False)
+        train_image.set_shape([IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        train_heatmap.set_shape([IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        validate_image.set_shape([IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        validate_heatmap.set_shape([IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
 
-            train_images = data_var(train_images_value)
-            train_heatmaps = data_var(train_heatmaps_value)
-            validate_images = data_var(validate_images_value)
-            validate_heatmaps = data_var(validate_heatmaps_value)
+        num_preprocess_threads = 2
+        min_queue_examples = 64
+        batch_images, batch_heatmaps = tf.train.batch(
+            [train_image, train_heatmap],
+            batch_size=BATCH_SIZE,
+            num_threads=num_preprocess_threads,
+            capacity=min_queue_examples + 3 * BATCH_SIZE)
 
-            train_images.set_shape([TRAIN_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
-            train_heatmaps.set_shape([TRAIN_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
-            validate_images.set_shape([VALIDATION_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
-            validate_heatmaps.set_shape([VALIDATION_SET_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
-
-            validate_images_augmented = augment_many(validate_images)
-
-        def initialize_images(sess, images, heatmaps):
-            # images_vars = [train_images, train_heatmaps, validate_images, validate_heatmaps]
-            # sess.run(
-            #     [var.initializer for var in images_vars],
-            #     feed_dict={images_initializer: images, heatmaps_initializer: heatmaps})
-            sess.run(tf.global_variables_initializer(),
-                     feed_dict={images_initializer: images, heatmaps_initializer: heatmaps})
-
-        self.initialize_images = initialize_images
-
-        with tf.name_scope('batch'):
-            batch_start = tf.placeholder(tf.int32, shape=[])
-            batch_images = tf.slice(train_images, [batch_start, 0, 0, 0],
-                                    [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
-            batch_heatmaps = tf.slice(train_heatmaps, [batch_start, 0, 0, 0],
-                                      [BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
-
-            augmented_batch_images = augment_many(batch_images)
-            augmented_batch_heatmaps = augment_many(batch_heatmaps)
+        augmented_batch_images = augment_many(batch_images)
+        augmented_batch_heatmaps = augment_many(batch_heatmaps)
 
         pred = conv_net(augmented_batch_images)
         ground_truth = tf.div(augmented_batch_heatmaps, 256)
@@ -289,64 +264,68 @@ class Model(object):
         correct_pred = tf.equal(tf.argmax(pred, 3), tf.argmax(ground_truth, 3))
         accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-        def train_on_batch(sess, batch_begin):
-            sess.run(optimizer, feed_dict={batch_start: batch_begin})
-            return sess.run([cost, accuracy], feed_dict={batch_start: batch_begin})
+        def train_on_batch(sess):
+            sess.run(optimizer)
+            return sess.run([cost, accuracy])
 
         self.train_on_batch = train_on_batch
 
-        # validation
-
-        tf.get_variable_scope().reuse_variables()
-
-        validation_pred = conv_net(validate_images_augmented)
-        validation_pred = tf.reshape(
-            validation_pred,
-            [-1, IMAGE_TRANSFORMATION_NUMBER, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
-        validation_pred = tf.map_fn(gather_transformations, validation_pred)
-        validation_ground_truth = tf.div(validate_heatmaps, 256)
-        validation_cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-            logits=validation_pred,
-            labels=validation_ground_truth))
-        validation_correct_pred = tf.equal(tf.argmax(validation_pred, 3), tf.argmax(validation_ground_truth, 3))
-        validation_accuracy = tf.reduce_mean(tf.cast(validation_correct_pred, tf.float32))
-
-        def validate(sess):
-            loss, acc = sess.run([validation_cost, validation_accuracy])
-            print("Validation loss %g" % loss)
-            print("Validation accuracy %g" % acc)
-
-        self.validate = validate
+        # # validation
+        #
+        # tf.get_variable_scope().reuse_variables()
+        #
+        # validation_pred = conv_net(validate_images_augmented)
+        # validation_pred = tf.reshape(
+        #     validation_pred,
+        #     [-1, IMAGE_TRANSFORMATION_NUMBER, IMAGE_SIZE, IMAGE_SIZE, CHANNELS])
+        # validation_pred = tf.map_fn(gather_transformations, validation_pred)
+        # validation_ground_truth = tf.div(validate_heatmaps, 256)
+        # validation_cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        #     logits=validation_pred,
+        #     labels=validation_ground_truth))
+        # validation_correct_pred = tf.equal(tf.argmax(validation_pred, 3), tf.argmax(validation_ground_truth, 3))
+        # validation_accuracy = tf.reduce_mean(tf.cast(validation_correct_pred, tf.float32))
+        #
+        # def validate(sess):
+        #     loss, acc = sess.run([validation_cost, validation_accuracy])
+        #     print("Validation loss %g" % loss)
+        #     print("Validation accuracy %g" % acc)
+        #
+        # self.validate = validate
 
     def train(self):
-        images, heatmaps = read_images()
-
         saver = tf.train.Saver(tf.trainable_variables())
 
         with tf.Session() as sess:
             writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
-            self.initialize_images(sess, images, heatmaps)
-            # sess.run(tf.variables_initializer(tf.trainable_variables()))
+            sess.run(tf.global_variables_initializer())
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
 
             try:
                 for i in count():
-                    for j, batch_begin in enumerate(range(0, TRAIN_SET_SIZE - BATCH_SIZE, BATCH_SIZE)):
-                        loss, acc = self.train_on_batch(sess, batch_begin)
+                    for j in range(0, TRAIN_SET_SIZE // BATCH_SIZE):
+                        loss, acc = self.train_on_batch(sess)
                         print("Iter " + str(j) + ", Minibatch Loss= " +
                               "{:.6f}".format(loss) + ", Training Accuracy= " +
                               "{:.5f}".format(acc))
 
                     # validate after every epoch
-                    self.validate(sess)
-                    saver.save(sess, 'save/model', global_step=i)
+                    # self.validate(sess)
+
+                    if i % 10 == 0:
+                        saver.save(sess, 'save/model', global_step=i)
 
                     # writer.add_summary(summaries)
                     # writer.close()
             except KeyboardInterrupt:
                 # TODO save model
                 print("Optimization Finished!")
-                self.validate(sess)
+                # self.validate(sess)
                 saver.save(sess, 'save/model', global_step=0)
+                coord.request_stop()
+                coord.join(threads)
 
 
 if __name__ == '__main__':
