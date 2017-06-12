89d1fcf153024ea7cf2539fdc1d2fee28e6dbc2f
PiotrJander <pjander2@gmail.com>
2017-06-12 22:12:27 +0200

    Adds dense layer and ops for accuracy.

--- zadanie-3/train.py
+++ zadanie-3/train.py
@@ -6,13 +6,12 @@ import datetime
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-
 HIDDEN_STATE_SIZE = 100
 CONCAT_SIZE = HIDDEN_STATE_SIZE + 28
 CONVEYOR_SIZE = 100
 LOG_DIR = 'logs/' + datetime.datetime.now().strftime("%B-%d-%Y;%H:%M")
 BATCH_SIZE = 50
-
+LEARNING_RATE = 1e-3
 
 weight = 'weight'
 bias = 'bias'
@@ -62,7 +61,7 @@ class Model(object):
 
                 return new_c, new_h, init_lsmt
 
-        def rnn_net(x, name='rnn_net'):
+        def rnn_net(x, name='lstm_net'):
             with tf.variable_scope(name):
                 x_rows = tf.unstack(x, axis=1)
                 c = tf.zeros(shape=[BATCH_SIZE, CONVEYOR_SIZE], dtype=tf.float32)
@@ -70,14 +69,24 @@ class Model(object):
                 for i, row in enumerate(x_rows):
                     c, h, init_lstm = lstm(c, h, row, reuse_variables=(i > 0))
 
+                with tf.variable_scope('final_dense'):
+                    weights = tf.get_variable('weights', shape=[HIDDEN_STATE_SIZE, 10],
+                                              initializer=tf.random_normal_initializer())
+                    bias = tf.get_variable('bias', shape=[1, 10], initializer=tf.zeros_initializer())
+                    ret = tf.matmul(h, weights) + bias
+
                 # noinspection PyUnboundLocalVariable
-                return h, init_lstm
+                return ret, init_lstm
 
-        run_batch, init_lstm = rnn_net(images_reshaped)
+        pred, init_lstm = rnn_net(images_reshaped)
 
-        self.init = tf.global_variables_initializer()
-        self.init_lstm = init_lstm
-        self.run_batch = run_batch
+        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=self.labels))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(self.cost)
+
+        correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(self.labels, 1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+        self.init = [tf.global_variables_initializer(), init_lstm]
 
         # first_input = tf.random_normal(shape=[BATCH_SIZE, 28])
         # second_input = tf.random_normal(shape=[BATCH_SIZE, 28])
@@ -99,9 +108,10 @@ class Model(object):
         with tf.Session() as sess:
             writer = tf.summary.FileWriter(LOG_DIR, sess.graph)
             batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
-            sess.run([self.init, self.init_lstm])
-            t = sess.run(self.run_batch, feed_dict={self.images: batch_x})
-            print(t)
+            sess.run(self.init)
+            _, cost, acc = sess.run([self.optimizer, self.cost, self.accuracy],
+                                    feed_dict={self.images: batch_x, self.labels: batch_y})
+            print(cost, acc)
 
 
 if __name__ == '__main__':
